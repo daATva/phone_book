59b6a04456a0df2d42fdb0b221d6f742c76f4cd9
daATva <riven1231@mail.ru>
2023-08-14 01:16:05 +0400

    fix pics

--- js/contacts.js
+++ js/contacts.js
@@ -39,7 +39,7 @@ function displayContacts() {
     let li = document.createElement("li");
     li.classList.add("contact-item");
     li.innerHTML = `
-      <img src="/img/check.png" alt="${contact.name}" class="contact-icon">
+      <img src="./img/check.png" alt="${contact.name}" class="contact-icon">
       <div class="contact-info">
         <p class="contact-name">${contact.name}</p>
         <p class="contact-phone">${contact.phone}</p>
