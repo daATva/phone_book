dbbb3abb0e3bac569e87f186dc0041fcf9ff8464
daATva <riven1231@mail.ru>
2023-08-20 23:50:47 +0400

    mmmm

--- js/contacts.js
+++ js/contacts.js
@@ -348,3 +348,18 @@ const produceBox = (produceFn) => {
 for(let i = 0; i < 25; i++) {
 	produceBox(boxFactory.produce);
 }
+
+
+function camelize(srt){
+  return srt
+    .split('-')
+    .map(
+      (word,index ) => index == 0 ? word : word[0].toUpperCase() + word.slice(1 )
+    )
+    .join('')
+}
+console.log(camelize("background-color") )
+
+
+camelize("list-style-image") 
+camelize("-webkit-transition")
