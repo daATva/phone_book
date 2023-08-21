55773ee73d93404f05a3bd3e3c322dda0e891f64
daATva <riven1231@mail.ru>
2023-08-21 23:41:59 +0400

    input fix

--- js/contacts.js
+++ js/contacts.js
@@ -93,26 +93,27 @@ function displayOrContacts() {
 }
 
 
+// Создаем переменную для хранения исходных контактов
+const originalsContacts = contacts;
+
 // Функция для фильтрации контактов по введенному тексту
 function filterContacts() {
     // Получаем текст из поля поиска
     let searchText = searchInput.value.toLowerCase();
-    // Фильтруем контакты по имени или телефону
-    const copiedContacts = JSON.parse(JSON.stringify(contacts));
     
-
-    contacts = copiedContacts.filter(contact => {
-      if (searchText === '') {
-        displayContacts();
-      }
-      return contact.name.toLowerCase().includes(searchText) || contact.phone.includes(searchText);
-          
-  
-    });
+    // Проверяем, если текст пустой, возвращаем исходные контакты
+    if (searchText === '') {
+        contacts = originalsContacts;
+    } else {
+        // Фильтруем контакты по имени или телефону
+        contacts = originalsContacts.filter(contact => {
+            return contact.name.toLowerCase().includes(searchText) || contact.phone.includes(searchText);
+        });
+    }
+    
     // Отображаем отфильтрованные контакты на странице
     displayContacts();
-    
-  }
+}
 
 // Функция для удаления контакта по имени
 function deleteContact(event) {
@@ -195,6 +196,7 @@ findButton.addEventListener('click',filterContacts)
 backButton.addEventListener('click',displayOrContacts)
 
 createButton.addEventListener("click", createContact);
+
 window.addEventListener("click", event => {
   if (event.target === modal) {
     closeModal();
@@ -209,157 +211,4 @@ displayContacts();
 // Поменять на поле действия + сделать проверку на инпут и каждый раз на при нажатиее 
 // На бекс пейс или делит , проверять на буквы и выводить , что подходит и записывать
 // А когда будет 0 символом , то высирать весь список
-// Я решал это задание , кеш обновился и оно пропало 
 
-// function createCounter(value) {
-//   let inc = () => value++;
-//   let dec = () => value--;
-//   let get = () => value;
-//   return { inc, dec, get };
-// }
-
-// const { inc, dec, get } = createCounter(5);
-// console.log(get()); // 5
-// inc();
-// inc();
-// inc();
-// dec();
-// console.log(get()); // 7
-
-// class Circle {
-//   constructor(radius) {
-//     this.radius = radius;
-//   }
-  
-//   getArea() {
-//     const pi = 3.14;
-//     const res = pi * this.radius ** 2;
-//     return res.toFixed(2);
-//   }
-// }
-
-// const circle = new Circle(5);
-// console.log(circle.getArea()); // "78.54"
-
-// class Product{
-//   constructor(name,price){
-//     this.name = name
-//     this.price = price
-//   }
-//   priceWithDiscount(proc) {
-//     const res = this.price - this.price / 100 * proc
-//     return res
-//   }
-// }
-
-// const product = new Product("Phone", 1000);
-// console.log(product.priceWithDiscount(10)); // 900
-// console.log(product.priceWithDiscount(20));
-// console.log(product.priceWithDiscount(70));
-
-// class Person {
-//   constructor(friends) {
-//   this.friends = [];
-//   }
-//   addFriend(name) {
-//   this.friends.push(name);
-//   }
-//   showFriends() {
-//   console.log(this.friends.join(', '));
-//   }
-//   }
-
-// const person = new Person();
-// person.addFriend("Иван");
-// person.addFriend("Сергей");
-// person.addFriend("Игорь");
-// person.showFriends(); // Иван, Сергей, Игорь
-
-// class Animal {
-//   constructor(name , favoriteFood){
-//     this.name = name
-//     this.food = favoriteFood
-//   }
-
-//   makeSound() {
-//     console.log("Animal sound");
-//   }
-
-//   sayName() {
-//       console.log("My name is " + this.name);
-//   }
-
-//   sayInfo() {
-//       console.log(this.name + "s favorite food is " + this.favoriteFood);
-//   }
-
-// }
-
-// class Cat extends Animal {
-//   makeSound() {
-//   console.log("Meow");
-//   }
-// }
-  
-// class Dog extends Animal {
-//   makeSound() {
-//   console.log("Gav!");
-//   }
-// }
-
-// const dog = new Dog('Rex', 'Meat');
-// const cat = new Cat('Barsik', 'Fish');
-
-// cat.makeSound(); // Meow
-// dog.makeSound(); // Gav!
-
-// dog.sayName(); // My name is Rex
-// cat.sayName(); // My name is Barsik
-
-// dog.sayInfo(); // Rex's favorite food is Meat
-// cat.sayInfo(); // Barsik's favorite food is Fish
-
-// const foo = {
-//   a:5,
-//   baz: function(){
-//     console.log(this.a)
-//   },
-//   bar:  () => {
-//     console.log(this.a)
-//   }
-// }
-
-// foo.bar();
-// foo.baz();
-const boxFactory = {
-	type: 'box',
-	count: 0,
-	produce: function() {
-		this.count++;
-		return 'Box №' + this.count;
-	}
-}
-
-const produceBox = (produceFn) => {
-	const boxName = produceFn();
-	console.log(boxName);
-}
-
-for(let i = 0; i < 25; i++) {
-	produceBox(boxFactory.produce);
-}
-
-
-function camelize(srt){
-  return srt
-    .split('-')
-    .map(
-      (word,index ) => index == 0 ? word : word[0].toUpperCase() + word.slice(1 )
-    )
-    .join('')
-}
-console.log(camelize("background-color") )
-
-
-camelize("list-style-image") 
-camelize("-webkit-transition")
